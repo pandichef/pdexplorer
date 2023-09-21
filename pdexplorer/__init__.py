051c7fe0b4b42397744e46667e20a34faa5fd90a
pandichef <rashtyda@gmail.com>
2023-09-21 13:39:41 -0500

    add chart aliases

--- pdexplorer/__init__.py
+++ pdexplorer/__init__.py
@@ -86,27 +86,6 @@ from .ai.streamlit import streamlit
 # from .dataset import Dataset
 # from .dataset import current
 from pdexplorer.dataset import current
-from pdexplorer.altair_mapper import (
-    histogram,
-    scatter,
-    arcchart,  # type: ignore
-    areachart,  # type: ignore
-    barchart,  # type: ignore
-    circlechart,  # type: ignore
-    geoshapechart,  # type: ignore
-    imagechart,  # type: ignore
-    linechart,  # type: ignore
-    pointchart,  # type: ignore
-    rectchart,  # type: ignore
-    rulechart,  # type: ignore
-    squarechart,  # type: ignore
-    textchart,  # type: ignore
-    tickchart,  # type: ignore
-    trailchart,  # type: ignore
-    boxplotchart,  # type: ignore
-    errorbandchart,  # type: ignore
-    errorbarchart,  # type: ignore
-)
 
 
 def methods():
@@ -124,3 +103,116 @@ def properties():
 
 
 # props = pformat(singleton.properties)
+
+
+from pdexplorer.altair_mapper import (
+    scatter,  # sc in Stata documentation
+    histogram as hist,
+    histogram as histo,
+    histogram as histog,
+    histogram as histogr,
+    histogram as histogra,
+    histogram as histogram,
+    arcchart as arc,  # type: ignore
+    arcchart as arcc,  # type: ignore
+    arcchart as arcch,  # type: ignore
+    arcchart as arccha,  # type: ignore
+    arcchart as arcchar,  # type: ignore
+    arcchart as arcchart,  # type: ignore
+    areachart as area,  # type: ignore
+    areachart as areac,  # type: ignore
+    areachart as areach,  # type: ignore
+    areachart as areacha,  # type: ignore
+    areachart as areachar,  # type: ignore
+    areachart as areachart,  # type: ignore
+    barchart as bar,  # type: ignore
+    barchart as barc,  # type: ignore
+    barchart as barch,  # type: ignore
+    barchart as barcha,  # type: ignore
+    barchart as barchar,  # type: ignore
+    barchart as barchart,  # type: ignore
+    circlechart as circle,  # type: ignore
+    circlechart as circlec,  # type: ignore
+    circlechart as circlech,  # type: ignore
+    circlechart as circlecha,  # type: ignore
+    circlechart as circlechar,  # type: ignore
+    circlechart as circlechart,  # type: ignore
+    geoshapechart as geoshape,  # type: ignore
+    geoshapechart as geoshapec,  # type: ignore
+    geoshapechart as geoshapech,  # type: ignore
+    geoshapechart as geoshapecha,  # type: ignore
+    geoshapechart as geoshapechar,  # type: ignore
+    geoshapechart as geoshapechart,  # type: ignore
+    imagechart as image,  # type: ignore
+    imagechart as imagec,  # type: ignore
+    imagechart as imagech,  # type: ignore
+    imagechart as imagecha,  # type: ignore
+    imagechart as imagechar,  # type: ignore
+    imagechart as imagechart,  # type: ignore
+    linechart as line,  # type: ignore
+    linechart as linec,  # type: ignore
+    linechart as linech,  # type: ignore
+    linechart as linecha,  # type: ignore
+    linechart as linechar,  # type: ignore
+    linechart as linechart,  # type: ignore
+    pointchart as point,  # type: ignore
+    pointchart as pointc,  # type: ignore
+    pointchart as pointch,  # type: ignore
+    pointchart as pointcha,  # type: ignore
+    pointchart as pointchar,  # type: ignore
+    pointchart as pointchart,  # type: ignore
+    rectchart as rect,  # type: ignore
+    rectchart as rectc,  # type: ignore
+    rectchart as rectch,  # type: ignore
+    rectchart as rectcha,  # type: ignore
+    rectchart as rectchar,  # type: ignore
+    rectchart as rectchart,  # type: ignore
+    rulechart as rule,  # type: ignore
+    rulechart as rulec,  # type: ignore
+    rulechart as rulech,  # type: ignore
+    rulechart as rulecha,  # type: ignore
+    rulechart as rulechar,  # type: ignore
+    rulechart as rulechart,  # type: ignore
+    squarechart as square,  # type: ignore
+    squarechart as squarec,  # type: ignore
+    squarechart as squarech,  # type: ignore
+    squarechart as squarecha,  # type: ignore
+    squarechart as squarechar,  # type: ignore
+    squarechart as squarechart,  # type: ignore
+    textchart as text,  # type: ignore
+    textchart as textc,  # type: ignore
+    textchart as textch,  # type: ignore
+    textchart as textcha,  # type: ignore
+    textchart as textchar,  # type: ignore
+    textchart as textchart,  # type: ignore
+    tickchart as tick,  # type: ignore
+    tickchart as tickc,  # type: ignore
+    tickchart as tickch,  # type: ignore
+    tickchart as tickcha,  # type: ignore
+    tickchart as tickchar,  # type: ignore
+    tickchart as tickchart,  # type: ignore
+    trailchart as trail,  # type: ignore
+    trailchart as trailc,  # type: ignore
+    trailchart as trailch,  # type: ignore
+    trailchart as trailcha,  # type: ignore
+    trailchart as trailchar,  # type: ignore
+    trailchart as trailchart,  # type: ignore
+    boxplotchart as boxplot,  # type: ignore
+    boxplotchart as boxplotc,  # type: ignore
+    boxplotchart as boxplotch,  # type: ignore
+    boxplotchart as boxplotcha,  # type: ignore
+    boxplotchart as boxplotchar,  # type: ignore
+    boxplotchart as boxplotchart,  # type: ignore
+    errorbandchart as errorband,  # type: ignore
+    errorbandchart as errorbandc,  # type: ignore
+    errorbandchart as errorbandch,  # type: ignore
+    errorbandchart as errorbandcha,  # type: ignore
+    errorbandchart as errorbandchar,  # type: ignore
+    errorbandchart as errorbandchart,  # type: ignore
+    errorbarchart as errorbar,  # type: ignore
+    errorbarchart as errorbarc,  # type: ignore
+    errorbarchart as errorbarch,  # type: ignore
+    errorbarchart as errorbarcha,  # type: ignore
+    errorbarchart as errorbarchar,  # type: ignore
+    errorbarchart as errorbarchart,  # type: ignore
+)
